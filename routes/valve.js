5d1771f83ec3d04ee262f49ae0b8908b9ba1e46c
marlinl <malin3369@gmail.com>
2021-01-23 21:18:36 +0800

    Add:take all of valva open and close api

--- routes/valve.js
+++ routes/valve.js
@@ -31,6 +31,22 @@ router.put('/test', async (ctx, next) => {
     ctx.body = result;
 });
 
+router.get('/takeOpen', async (ctx) => {
+    let pina = 37;
+    let pinb = 35;
+    gpio.outPutHigh(pina);
+    gpio.outPutHigh(pinb)
+    ctx.body = "success";
+});
+
+router.get('/takeClose', async (ctx) => {
+    let pina = 37;
+    let pinb = 35;
+    gpio.outPutLow(pina);
+    gpio.outPutLow(pinb)
+    ctx.body = "success";
+});
+
 router.put('/demo', async (ctx, next) => {
     let request = ctx.request;
     let reqBody = request.body;
