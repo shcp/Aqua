7f34273c1add56ad21d79fdacb8839733276482e
marlinl <malin3369@gmail.com>
2021-01-24 14:51:50 +0800

    Fix:update schedule

--- core/schedule.js
+++ core/schedule.js
@@ -8,17 +8,18 @@ const smallValve = 35;
 var job = schedule.scheduleJob("0/10 * * * * *", function(){
     var hour = moment().hour();
     var minute = moment().minute();
-    if (hour == 8 && minute == 0){
+    var dayOfWeek = moment().weekday();
+    if (dayOfWeek == 0 && hour == 8 && minute == 0){
         gpio.outPutHigh(bigValve);
         gpio.outPutHigh(smallValve);
         console.log('open valve :'+moment().format());
     }
-    if (hour == 8 && minute == 3){
+    if (dayOfWeek == 0 && hour == 8 && minute == 30){
         gpio.outPutLow(bigValve);
         gpio.outPutLow(smallValve);
         console.log('close valve :'+moment().format());
     }
-    if (hour == 0 && minute == 33){
+    if (dayOfWeek == 0 && hour == 14 && minute == 50){
         console.log('test :'+moment().format());
     }
  });
